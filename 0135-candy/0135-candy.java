bb1773ade44f44c3c155abb7428bc1fcdd4d2c25
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-04-27 18:02:15 +0530

    Time: 3 ms (63.95%), Space: 43.4 MB (57.78%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int candies[] = new int[n];
+        if (n == 1) {
+            return 1;
+        }
+
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        int totalCandies = 0;
+
+        for (int i = 0; i < n; i++) {
+            totalCandies += candies[i];
+        }
+
+        return totalCandies;
+    }
+}
