811cf50d3bc58b17ca072a3534189f92102c810d
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-06-07 00:58:49 +0530

    Time: 1 ms (97.34%), Space: 44.4 MB (30.95%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,42 +1,31 @@
 class Solution {
     
     public int maxProfit(int[] prices) {
+    
         int n = prices.length;
         
-        int dp[][] = new int[n][2];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        int dp[][] = new int[n+1][2];
+        
+        for(int i=0;i<=1;i++){
+            dp[n][i] = 0;
         }
         
-        return buySell(0,0,prices,n,dp);
-    }
-    
-    public int buySell(int ind,int buy,int prices[],int n,int dp[][]){
-        
-        if(ind==n){
-            return 0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                
+                int profit;
+                
+                if(buy==0){
+                    profit = Math.max(0+dp[ind+1][buy],-prices[ind]+dp[ind+1][1-buy]);
+                }else{
+                    profit = Math.max(0+dp[ind+1][buy],prices[ind]+dp[ind+1][1-buy]);
+                }
+                dp[ind][buy] = profit;
+            }
         }
         
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
-        }
+        return dp[0][0];
         
-        int op1;
-        int op2;
-        
-        if(buy==0){
-            //not buy
-           op1 = buySell(ind+1,buy,prices,n,dp);
-            //buy
-           op2 = -prices[ind]+buySell(ind+1,buy+1,prices,n,dp);
-        }
-        else{
-            //not sell
-            op1 = buySell(ind+1,buy,prices,n,dp);
-            op2 = prices[ind]+buySell(ind+1,buy-1,prices,n,dp);
-        }
-        
-        return dp[ind][buy] = Math.max(op1,op2);
     }
     
 }
