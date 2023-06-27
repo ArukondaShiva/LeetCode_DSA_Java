5a5f80124ce6d539350f37fc4650c7d7c898191b
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-06-27 21:57:59 +0530

    Added solution - LeetHub

--- Find the number of islands - GFG/find-the-number-of-islands.java
+++ Find the number of islands - GFG/find-the-number-of-islands.java
@@ -38,84 +38,69 @@ class Pair{
 }
 
 class Solution {
-    
-    public int numIslands(char[][] grid) {
      
-       int n = grid.length;
-       int m = grid[0].length;
-       
-       int vis[][] = new int[n][m];
-       
-       for(int r[]: vis){
-           Arrays.fill(r,0);
-       }
-       
-       int islands = 0;
-       
-       for(int i=0;i<n;i++){
-           for(int j=0;j<m;j++){
-               if(grid[i][j]=='1' && vis[i][j]==0){
-                   islands++;
-                   bfs(i,j,grid,vis);
-               }
-           }
-       }
+    public int numIslands(char[][] grid) {
         
-       return islands;
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        int vis[][] = new int[n][m];
+        
+        for(int row[]:vis){
+            Arrays.fill(row,0);
+        }
+        
+        int islands = 0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    islands++;
+                    bfs(i,j,vis,grid);   
+                }
+                
+            }
+        }
+        
+        return islands;
     }
     
-    public void bfs(int row,int col,char grid[][],int vis[][]){
+    public void bfs(int row,int col,int vis[][],char grid[][]){
         
-         vis[row][col] = 1;
+        vis[row][col] = 1;
         
-         Queue<Pair> q = new LinkedList<>();
-         
-         int n = grid.length;
-         int m = grid[0].length;
-         
-         q.add(new Pair(row,col));
-         
-         while(!q.isEmpty()){
-             
-              row = q.peek().row;
-              col = q.peek().col;
-             
-             q.poll();
-             
-             for(int r=-1;r<=1;r++){
-                 for(int c=-1;c<=1;c++){
-                     
-                     int nrow = row+r;
-                     int ncol = col+c;
-                     
-                     if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0){
-                         q.add(new Pair(nrow,ncol));
-                         vis[nrow][ncol] = 1;
-                     }
-                     
-                 }
-             }
-             
-         }
-         
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(row,col));
+        
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        while(!q.isEmpty()){
+            
+            row = q.peek().row;
+            col = q.peek().col;
+            q.poll();
+            
+            for(int r=-1;r<=1;r++){
+                for(int c=-1;c<=1;c++){
+                    int nrow = row+r;
+                    int ncol = col+c;
+                    
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && vis[nrow][ncol]==0){
+                        vis[nrow][ncol] = 1;
+                        bfs(nrow,ncol,vis,grid);
+                    }
+                    
+                }
+            }
+            
+        }
+        
     }
     
     
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
