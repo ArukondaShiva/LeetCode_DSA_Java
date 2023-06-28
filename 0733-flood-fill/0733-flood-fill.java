defde8a8e7ad003858a9f18fb6f32bd188783079
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-06-28 10:24:31 +0530

    Time: 1 ms (46.37%), Space: 44.1 MB (39.83%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,45 +1,54 @@
+class Pair{
+    int row;
+    int col;
+    Pair(int r,int c){
+        row = r;
+        col = c;
+    }
+}
+
 class Solution {
-    
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
-        int iniColor = image[sr][sc];
-        
         int n = image.length;
         int m = image[0].length;
-        
-        int ans[][] = image;
+
         int vis[][] = new int[n][m];
-        
-        for(int row[] : vis){
-            Arrays.fill(row,0);
+
+        for (int row[] : vis) {
+            Arrays.fill(row, 0);
         }
-        
-        DFS(sr,sc,iniColor,color,ans,vis);
-            
+
+        int ans[][] = image;
+
+        int iniColor = image[sr][sc];
+
+        int delrow[] = { -1, 0, +1, 0 };
+        int delcol[] = { 0, +1, 0, -1 };
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(sr, sc));
+        vis[sr][sc] = 1;
+        ans[sr][sc] = color;
+
+        while (!q.isEmpty()) {
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && ans[nrow][ncol] == iniColor && vis[nrow][ncol] == 0) {
+                    vis[nrow][ncol] = 1;
+                    q.add(new Pair(nrow, ncol));
+                    ans[nrow][ncol] = color;
+                }
+            }
+        }
+
         return ans;
     }
-    
-    public void DFS(int row,int col,int iniColor,int color,int ans[][],int vis[][]){
-        
-        vis[row][col] = 1;
-        ans[row][col] = color;
-        
-        int n = ans.length;
-        int m = ans[0].length;
-        
-        int delrow[] = {-1,0,+1,0};
-        int delcol[] = {0,+1,0,-1};
-        
-        for(int i=0;i<4;i++){
-            int nrow = row+delrow[i];
-            int ncol = col+delcol[i];
-            
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && ans[nrow][ncol]==iniColor && vis[nrow][ncol]==0){
-                DFS(nrow,ncol,iniColor,color,ans,vis);
-            }
-            
-        }
-        
-    }
-    
 }
