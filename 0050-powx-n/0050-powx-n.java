0aaa47e2e1c0d1ab932768cebedcd21920fe1204
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-06-30 15:55:51 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (89.08%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    public double myPow(double x, int n) {
+        
+        double ans=1.0;
+        long nn=n;
+        
+        if(nn<0){
+          nn = (-1)*nn;
+        }
+        
+        while(nn>0){
+            
+          if(nn%2==0){
+            nn = nn/2;
+            x=x*x;
+          }
+          else if(nn%2==1){
+            nn = nn-1;
+            ans = ans*x;
+          }
+        }
+        
+        if(n<0){
+            ans = ((double)1.0/(double)(ans));
+        }
+        
+        return ans;   
+    }
+    
+}
