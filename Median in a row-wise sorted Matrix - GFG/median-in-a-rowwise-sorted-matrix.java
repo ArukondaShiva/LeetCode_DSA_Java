c1335716fac67a54d5c0e47490f294f9851d2a48
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-07-23 23:04:23 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    int median(int matrix[][], int R, int C) {
+      
+      int N = R*C;
+      
+      int start = 1;
+      int end = 2000;
+      
+      while(start<=end){
+          
+          int mid = (start+end)/2;
+          int smallerElements = findSmallerElements(matrix,mid);
+          
+          if(smallerElements<=N/2){
+              start = mid+1;
+          }
+          else{
+              end = mid-1;
+          }
+      }
+      
+      return start;        
+    }
+    
+    int findSmallerElements(int matrix[][],int mid){
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int count = 0;
+        
+        for(int r[] : matrix){
+            count+= binarySearch(r,mid);
+        }
+        
+        return count;
+    }
+    
+    int binarySearch(int arr[],int target){
+         int n = arr.length;
+         
+         int low = 0;
+         int high = n-1;
+         
+         while(low<=high){
+             int mid = (low+high)/2;
+             if(arr[mid]<=target){
+                 low = mid+1;
+             }
+             else{
+                 high = mid-1;
+             }
+         }
+         return low;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
