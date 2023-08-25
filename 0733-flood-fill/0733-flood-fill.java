6682285b981a093a1f64da3730e43a4781424352
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-08-25 17:47:07 +0530

    Time: 1 ms (49.41%), Space: 44.1 MB (44.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,7 @@
-class Pair{
-    int row;
-    int col;
-    Pair(int _row,int _col){
-        row = _row;
-        col = _col;
-    }
-}
-
 class Solution {
     
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        
+    
         int n = image.length;
         int m = image[0].length;
         
@@ -22,44 +13,31 @@ class Solution {
             Arrays.fill(r,0);
         }
         
-        BFS(sr,sc,image,visited,color,iniColor);
+        DFS(sr,sc,image,visited,iniColor,color);
         
         return image;
     }
     
-    public void BFS(int sr,int sc,int image[][],int visited[][],int newColor,int iniColor){
+    public void DFS(int row,int col,int image[][],int visited[][],int iniColor,int newColor){
         
-        visited[sr][sc] = 1;
-        image[sr][sc] = newColor;
+        visited[row][col] = 1;
+        image[row][col] = newColor;
         
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(sr,sc));
-        
-        int delrow[] = {-1,0,+1,0};
-        int delcol[] = {0,+1,0,-1};
         int n = image.length;
         int m = image[0].length;
         
-        while(!q.isEmpty()){
-            Pair p = q.peek();
-            q.poll();
+        int delrow[] = {-1,0,+1,0};
+        int delcol[] = {0,+1,0,-1};
+        
+        for(int i=0;i<4;i++){
+            int nrow = row+delrow[i];
+            int ncol = col+delcol[i];
             
-            int row = p.row;
-            int col = p.col;
-            
-            for(int i=0;i<4;i++){
-                int nrow = row+delrow[i];
-                int ncol = col+delcol[i];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && visited[nrow][ncol]==0){
-                    visited[nrow][ncol] = 1;
-                    image[nrow][ncol] = newColor;
-                    q.add(new Pair(nrow,ncol));
-                }
-                
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && visited[nrow][ncol]==0){
+                DFS(nrow,ncol,image,visited,iniColor,newColor);
             }
-            
         }
         
     }
+    
 }
