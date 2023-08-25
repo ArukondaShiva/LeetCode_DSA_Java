40df6d5fe7d4f8b762237fbb2621b961efd66d4d
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-08-25 17:33:17 +0530

    Time: 2 ms (14.91%), Space: 43.9 MB (74.46%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,54 +1,65 @@
 class Pair{
     int row;
     int col;
-    Pair(int r,int c){
-        row = r;
-        col = c;
+    Pair(int _row,int _col){
+        row = _row;
+        col = _col;
     }
 }
 
 class Solution {
-
+    
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
         int n = image.length;
         int m = image[0].length;
-
-        int vis[][] = new int[n][m];
-
-        for (int row[] : vis) {
-            Arrays.fill(row, 0);
-        }
-
-        int ans[][] = image;
-
+        
         int iniColor = image[sr][sc];
-
-        int delrow[] = { -1, 0, +1, 0 };
-        int delcol[] = { 0, +1, 0, -1 };
-
-        Queue<Pair> q = new LinkedList<>();
-
-        q.add(new Pair(sr, sc));
-        vis[sr][sc] = 1;
-        ans[sr][sc] = color;
-
-        while (!q.isEmpty()) {
-            int row = q.peek().row;
-            int col = q.peek().col;
-            q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && ans[nrow][ncol] == iniColor && vis[nrow][ncol] == 0) {
-                    vis[nrow][ncol] = 1;
-                    q.add(new Pair(nrow, ncol));
-                    ans[nrow][ncol] = color;
-                }
-            }
+        
+        int visited[][] = new int[n][m];
+        
+        for(int r[] : visited){
+            Arrays.fill(r,0);
         }
-
-        return ans;
+        
+        BFS(sr,sc,image,visited,color,iniColor);
+        
+        return image;
+    }
+    
+    public void BFS(int sr,int sc,int image[][],int visited[][],int newColor,int iniColor){
+        
+        visited[sr][sc] = 1;
+        image[sr][sc] = newColor;
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr,sc));
+        
+        int delrow[] = {-1,0,+1,0};
+        int delcol[] = {0,+1,0,-1};
+        int n = image.length;
+        int m = image[0].length;
+        
+        while(!q.isEmpty()){
+            Pair p = q.peek();
+            q.poll();
+            
+            int row = p.row;
+            int col = p.col;
+            
+            for(int i=0;i<4;i++){
+                int nrow = row+delrow[i];
+                int ncol = col+delcol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==iniColor && visited[nrow][ncol]==0){
+                    visited[nrow][ncol] = 1;
+                    image[nrow][ncol] = newColor;
+                    q.add(new Pair(nrow,ncol));
+                }
+                
+            }
+            
+        }
+        
     }
 }
