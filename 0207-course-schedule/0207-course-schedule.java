9aa0dd53a998580082d9f174630651710b46cd8e
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2023-08-29 18:39:42 +0530

    Time: 7 ms (29.45%), Space: 44.6 MB (17.49%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,71 @@
+class Solution {
+    
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+    
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<Integer>());
+        }
+        
+        for(int r[] : prerequisites){
+            adj.get(r[0]).add(r[1]);
+        }
+        
+        int indegree[] = new int[numCourses];
+        Arrays.fill(indegree,0);
+        
+        for(int i=0;i<numCourses;i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+        
+        int count = 0;
+        
+        while(!q.isEmpty()){
+            
+            int node = q.poll();
+            count++;
+            
+            for(int next : adj.get(node)){
+                
+                indegree[next]--;
+                if(indegree[next]==0){
+                    q.add(next);
+                }
+            }
+        }
+        
+        
+        return count==numCourses;
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
