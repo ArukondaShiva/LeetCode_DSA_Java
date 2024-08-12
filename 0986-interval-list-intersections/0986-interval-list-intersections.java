89bc36ed6fc341b15b40ce820fe5045e5732d3ce
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-08-13 00:27:26 +0530

    Time: 5 ms (21.07%), Space: 45.5 MB (39.32%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,36 +1,31 @@
 class Solution {
     
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-    
+        
         List<int[]> resList = new ArrayList<>();
+        
         int i = 0;
         int j = 0;
         
         while(i<firstList.length && j<secondList.length){
             
-            int firstStart = firstList[i][0];
-            int firstEnd = firstList[i][1];
+            int latestStart = Math.max(firstList[i][0] , secondList[j][0]);
+            int earliestEnd = Math.min(firstList[i][1] , secondList[j][1]);
             
-            int secondStart = secondList[j][0];
-            int secondEnd = secondList[j][1];
-            
-            int latestST = Math.max(firstStart,secondStart);
-            int earliestET = Math.min(firstEnd,secondEnd);
-            
-            if(latestST<=earliestET){
-                resList.add(new int[]{latestST,earliestET});
+            if(latestStart <= earliestEnd){
+                resList.add(new int[]{latestStart,earliestEnd});
             }
-             
-            if(earliestET==firstEnd){
+            
+            if(earliestEnd == firstList[i][1]){
                 i++;
             }else{
                 j++;
             }
-                
         }
         
-        int[][] resArr = new int[resList.size()][2];
+        int[][] resArr =  new int[resList.size()][2];
         int k = 0;
+        
         for(int r[] : resList){
             resArr[k][0] = r[0];
             resArr[k][1] = r[1];
