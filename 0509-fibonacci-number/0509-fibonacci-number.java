648fa61f0950ef4a6bc51894de68887a6c7c9311
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-11-14 00:52:40 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (66.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,24 +1,21 @@
 class Solution {
     
     public int fib(int n) {
-        
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return findFibonacci(n,dp);
-    }
     
-    public int findFibonacci(int n,int dp[]){
-        
         if(n<=1){
             return n;
         }
         
-        if(dp[n]!=-1){
-            return dp[n];
+        int dp[] = new int[n+1];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
         
-        
-        return dp[n] = findFibonacci(n-1,dp)+findFibonacci(n-2,dp);
+        return dp[n];
     }
     
 }
