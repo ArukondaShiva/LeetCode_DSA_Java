e524c3b3ea85a84d2472398347b39796683962f6
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-11-16 00:07:53 +0530

    Time: 4 ms (53.49%), Space: 44.3 MB (38.71%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -5,26 +5,25 @@ class Solution {
     
         int n = triangle.size();
         int dp[][] = new int[n][n];
-        for(int r[] : dp){
-            Arrays.fill(r,-1);
+        
+        for(int i=n-1;i>=0;i--){
+            
+            for(int j=0;j<=i;j++){
+            
+                if(i==n-1){
+                    dp[i][j] = triangle.get(i).get(j);
+                    continue;
+                }
+                
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                
+                dp[i][j] = Math.min(down,diagonal);
+            }
+            
         }
-        return findMin(0,0,n,triangle,dp);
-    }
-    
-    public int findMin(int i,int j,int n,List<List<Integer>> triangle,int dp[][]){
         
-        if(i==n-1){
-            return triangle.get(i).get(j);
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        int down = triangle.get(i).get(j) + findMin(i+1,j,n,triangle,dp);
-        int diagonal = triangle.get(i).get(j) + findMin(i+1,j+1,n,triangle,dp);
-        
-        return dp[i][j] = Math.min(down,diagonal);
+        return dp[0][0];
     }
     
 }
