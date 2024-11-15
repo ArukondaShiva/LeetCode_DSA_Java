fc56fa2685458e41aed2743399d0d62f35136ca8
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-11-15 23:40:18 +0530

    Time: 5 ms (31.90%), Space: 47.5 MB (44.65%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -7,66 +7,40 @@ class Solution {
         
         int dp[][] = new int[n][m];
         
-        /*
-        for(int r[] : dp){
-            Arrays.fill(r,-1);
-        }
-        return findMinPathSum(n-1,m-1,grid,dp);
-        */
-        
-        
         for(int i=0;i<n;i++){
             
             for(int j=0;j<m;j++){
                 
                 if(i==0 && j==0){
                     dp[i][j] = grid[i][j];
-                    continue;
-                }
-                
-                int up = grid[i][j];
-                
-                if(i>0){
-                    up += dp[i-1][j];
                 }else{
-                    up += (int)(1e9);
+                    
+                    int up = grid[i][j];
+                    
+                    if(i>0){
+                        up += dp[i-1][j];
+                    }else{
+                        up += (int)(1e9);
+                    }
+                    
+                    int left = grid[i][j];
+                    
+                    if(j>0){
+                        left += dp[i][j-1];
+                    }else{
+                        left += (int)(1e9);
+                    }
+                    
+                    dp[i][j] = Math.min(up,left);
                 }
                 
-                int left = grid[i][j] ;
-                
-                if(j>0){
-                    left += dp[i][j-1];
-                }else{
-                    left += (int)(1e9);
-                }
-                
-                dp[i][j] = Math.min(up,left);
             }
+            
         }
         
         return dp[n-1][m-1];
     }
     
-    
-    
-    public int findMinPathSum(int i,int j,int grid[][],int dp[][]){
-        
-        if(i==0 && j==0){
-            return grid[0][0];
-        }
-        
-        if(i<0 || j<0){
-            return (int)1e9;
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        int up = grid[i][j] + findMinPathSum(i-1,j,grid,dp);
-        int left = grid[i][j] + findMinPathSum(i,j-1,grid,dp);
-        
-        return dp[i][j] = Math.min(up,left);
-    }
+
     
 }
