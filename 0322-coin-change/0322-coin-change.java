a3c96777621e0fe51d8e6085e26700c353e86ce6
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-11-20 15:48:56 +0530

    Time: 15 ms (77.50%), Space: 45.3 MB (17.04%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,39 +1,41 @@
 class Solution {
     
     public int coinChange(int[] coins, int amount) {
+        
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        for(int r[] : dp){
-            Arrays.fill(r,-1);
-        }
-        int ans = findCoinChange(n-1,coins,amount,dp);
-        return ans==(1e9) ? -1 : ans;
-    }
-    
-    public int findCoinChange(int ind,int coins[],int amount,int dp[][]){
         
-        if(ind==0){
-            if(amount%coins[0]==0){
-                return amount/coins[0];
+        for(int am=0;am<=amount;am++){
+            if(am%coins[0]==0){
+                dp[0][am] = am/coins[0];
             }else{
-                return (int)(1e9);
+                dp[0][am] = (int)(1e9);
             }
         }
         
-        
-        if(dp[ind][amount]!=-1){
-            return dp[ind][amount];
+        for(int ind=1;ind<n;ind++){
+            
+            for(int target=0;target<=amount;target++){
+                
+                int notTaken = 0+dp[ind-1][target];
+                
+                int taken = (int)(1e9);
+                
+                if(coins[ind]<=target){
+                    taken = 1+dp[ind][target-coins[ind]];
+                }
+                
+                dp[ind][target] = Math.min(notTaken,taken);
+            }
         }
         
-        int notTaken = findCoinChange(ind-1,coins,amount,dp);
+        int ans = dp[n-1][amount];
         
-        int taken = (int)(1e9);
-        
-        if(coins[ind]<=amount){
-            taken = 1+findCoinChange(ind,coins,amount-coins[ind],dp);
+        if(ans==(int)(1e9)){
+            return -1;
+        }else{
+            return ans;
         }
-        
-        return dp[ind][amount] = Math.min(notTaken,taken);
     }
     
 }
