6b62a4abd3850327a6dc1a239ec699412ee9b47c
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-11-23 20:47:39 +0530

    Time: 18 ms (70.55%), Space: 45 MB (86.47%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,27 +4,41 @@ class Solution {
         int n1 = s.length();
         int n2 = t.length();
         
-        int dp[][] = new int[n1+1][n2+1];
+        //int dp[][] = new int[n1+1][n2+1];
+        int prev[] = new int[n2+1];
         
+        
+        /*
         for(int j=0;j<=n2;j++){
             dp[0][j] = 0;
         }
        
         for(int i=0;i<=n1;i++){
             dp[i][0] = 1;
-        }
+        }*/
+        
+        prev[0] = 1;
         
         for(int i=1;i<=n1;i++){
+            
+            int cur[] = new int[n2+1];
+            cur[0] = 1;
+            
             for(int j=1;j<=n2;j++){
                 if(s.charAt(i-1)==t.charAt(j-1)){
-                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                    //dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                    cur[j] = prev[j-1]+prev[j];
                 }else{
-                    dp[i][j] = dp[i-1][j];
+                    //dp[i][j] = dp[i-1][j];
+                    cur[j] = prev[j];
                 }
             }
+            
+            prev = cur;
         }
         
-        return dp[n1][n2];
+        //return dp[n1][n2];
+        return prev[n2];
     }
     
 }
