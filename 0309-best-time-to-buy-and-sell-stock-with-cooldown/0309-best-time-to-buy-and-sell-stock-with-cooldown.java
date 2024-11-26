4d4a32e74e922ec5be21a8e8981617b3129a7b08
Arukonda Shiva <49832102+ArukondaShiva@users.noreply.github.com>
2024-11-26 09:13:24 +0530

    Time: 1 ms (83.86%), Space: 41.7 MB (59.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,37 +1,25 @@
 class Solution {
     
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int dp[][] = new int[n][2];
-        for(int r[] : dp){
-            Arrays.fill(r,-1);
+        int n = prices.length; 
+        int dp[][] = new int[n+2][2];
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int op1,op2;
+                if(buy==0){
+                    op1 = 0 + dp[ind+1][0];
+                    op2 = -prices[ind] + dp[ind+1][1];
+                }else{
+                    op1 = 0 + dp[ind+1][1];
+                    op2 = prices[ind] + dp[ind+2][0];
+                }
+                dp[ind][buy] = Math.max(op1,op2);
+            }
         }
-        return findMaxProfit(0,0,n,prices,dp);       
+        
+        return dp[0][0];
     }
     
-    public int findMaxProfit(int ind,int buy,int n,int prices[],int dp[][]){
-        
-        if(ind>=n){
-            return 0;
-        }
-        
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
-        }
-        
-        int op1;
-        int op2;
-        
-        if(buy==0){ // buy
-            op1 = 0+findMaxProfit(ind+1,0,n,prices,dp);
-            op2 = -prices[ind] + findMaxProfit(ind+1,1,n,prices,dp);
-        } 
-        else{
-            op1 = 0+findMaxProfit(ind+1,1,n,prices,dp);
-            op2 = prices[ind] + findMaxProfit(ind+2,0,n,prices,dp);
-        }
-        
-        return dp[ind][buy] = Math.max(op1,op2);
-    }
-    
+   
 }
